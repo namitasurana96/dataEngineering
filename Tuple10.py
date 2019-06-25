cfdb5525ab78429a6241ef43a284a8dc0d54de38
Namita Surana <surananamita96@gmail.com>
2019-06-25 09:45:13 +0530

    Tuple progras

---
+++ Tuple10.py
@@ -0,0 +1,3 @@
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+print(tuplex)
+print(tuplex[::-1])
---
+++ Tuple9.py
@@ -0,0 +1,11 @@
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+slice = tuplex[3:5]
+print(slice)
+slice=tuplex[-8:-4]
+print(slice)
+slice = tuplex[2:9:2]
+print(slice)
+slice=tuplex[::4]
+print(slice)
+slice = tuplex[9:2:-4]
+print(slice)
