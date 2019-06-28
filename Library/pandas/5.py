e99ce3c59b3574118c560462a3f2771e20ae4176
Namita Surana <surananamita96@gmail.com>
2019-06-28 16:47:27 +0530

    Python Library

--- Library/pandas/5.py
+++ Library/pandas/5.py
@@ -1,4 +1,11 @@
 import pandas as pd
-dict={'a':10, 'b':100, 'c':500}
-print(dict)
-print(pd.Series(dict))
+import numpy as np
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+print(exam_data)
+df=pd.DataFrame(exam_data,['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+print(df)
---
+++ Library/pandas/6.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+arr=np.array([1,2,3,4,5])
+print(arr)
+print(pd.Series(arr))
